4759346762988bfa020cb74f3b320bb0b39f16e7
yaans <kbeeysk@naver.com>
2020-08-09 22:09:52 +0900

    error response 참고 링크, enum으로 작성

--- spring-security/yaans/spring-security/src/main/java/edu/spring/security/response/ErrorResponse.java
+++ spring-security/yaans/spring-security/src/main/java/edu/spring/security/response/ErrorResponse.java
@@ -3,6 +3,28 @@ package edu.spring.security.response;
 /**
  * 참고
  * https://cheese10yun.github.io/spring-guide-exception/
+ *
+ * RESTful 서비스 설계와 개발 - 메시지와 예외 처리
+ * https://eblo.tistory.com/48
+ *
+ * Spring Boot, REST API 예외 응답 구조 설계하기
+ * https://jsonobject.tistory.com/501
+ *
+ * Spring Boot, ExceptionHandler, 전역 예외 처리 로직 작성하기
+ * https://javafactory.tistory.com/1560?category=618813
  */
-public class ErrorResponse {
+public enum ErrorResponse {
+
+    TEST(200,"","");
+
+    private int status;
+    private String code;
+    private String message;
+
+    ErrorResponse(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+//    public static ErrorResponse test= new ErrorResponse(200, "dsf","dfs");
 }
